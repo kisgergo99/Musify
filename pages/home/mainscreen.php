a12085d7d8d71307fad5ab80d64d6276a85edd5b
Tóth Gergő <tgery3@gmail.com>
2021-10-25 18:46:15 +0200

    Logout mukszik

--- pages/home/mainscreen.php
+++ pages/home/mainscreen.php
@@ -4,4 +4,7 @@ $usertype = new Usertype();
 
 <body>
     <h1>Welcome back! <?php echo($usertype->getUsername()); ?> :)</h1>
+    <form action="logout.php" method="POST" enctype='application/x-www-form-urlencoded'>
+        <input type="submit" name="kijelentkezes" id="kijelentkezes" value="Sign Out" class="btn btn-primary"/>
+    </form>
 </body>
--- pages/landing/login.php
+++ pages/landing/login.php
@@ -1,7 +1,7 @@
 <?php
 
 $usertype = new Usertype();
-if($usertype->checkCaptcha(htmlspecialchars($_POST['captcha_code'], ENT_QUOTES, 'UTF-8'))){
+if(isset($_POST['bejelentkezes']) && $usertype->checkCaptcha(htmlspecialchars($_POST['captcha_code'], ENT_QUOTES, 'UTF-8'))){
 	if(isset($_POST['bejelentkezes']) && $_POST['user_email'] != NULL && $_POST['passw'] != NULL){
 		$passw = $_POST['passw'];
 		$user = htmlspecialchars($_POST['user_email'], ENT_QUOTES, 'UTF-8');
@@ -11,7 +11,7 @@ if($usertype->checkCaptcha(htmlspecialchars($_POST['captcha_code'], ENT_QUOTES, 
 		if($user != NULL && $passw != NULL &&
 		($user == $usertype->getUserByEmail($user)['user_email'] && 
 		(password_verify($passw, $usertype->getUserByEmail($user)['user_password']))) && $captcha == $_SESSION['captcha_for_user']){
-			$usertype->createLoginSession($user, $captcha);
+			$usertype->createLoginSession($usertype->getUserByEmail($user)['username'], $captcha);
 			header('Location: '. "./index.php");
 			die();
 		}else{
@@ -22,13 +22,5 @@ if($usertype->checkCaptcha(htmlspecialchars($_POST['captcha_code'], ENT_QUOTES, 
 }else{
 	header('Location: '. "./index.php?status=loginfailed");
 	die();
-}
-
-if(isset($_POST['kijelentkezes'])){
-	if($usertype->isLoggedIn()){
-		$usertype->dropSession();
-		header('Location: '. "../index.php");
-		die();
-	}
 }
 

ece00d406f68aaf78badc33578f7c6ce7ec4a02f
Tóth Gergő <tgery3@gmail.com>
2021-10-25 18:52:45 +0200

    Sometimes things get complicated

--- index.php
+++ index.php
@@ -9,6 +9,13 @@ if(isset($_GET['status'])){
 switch($statuscode){
     default:
         include(__DIR__."/Controller/Selector.php");
+        break;
+    case "loginfailed":
+        echo "<div class='alert alert-danger' role='alert'>
+                <center>Wrong email or password! Please try again..</center>
+                </div>";
+        include(__DIR__."/Controller/Selector.php");
+        break;
 }
 
 ?>

f307e10806e15e97735405ee74b017034c8ff077
Tóth Gergő <tgery3@gmail.com>
2021-10-29 23:45:36 +0200

    Designed webplayer

--- pages/home/mainscreen.php
+++ pages/home/mainscreen.php
@@ -2,7 +2,7 @@
 $usertype = new Usertype();
 ?>
 <body class="mainscreenframe">
-<nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark">
+<nav class="p-3 navbar sticky-top navbar-expand-md navbar-dark bg-dark">
   <a class="navbar-brand" href="#">SZE-Musify</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-bs-target="#myNavLol" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -16,7 +16,7 @@ $usertype = new Usertype();
         <a class="nav-link clickableMenu" href="javascript:void(0);" id="menu_albums"><i class="fa fa-align-justify" aria-hidden="true"></i> Albums</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link clickableMenu" href="javascript:void(0);" id="menu_search"><i class="fa fa-search" aria-hidden="true"></i> Search</a>
+        <a class="nav-link clickableMenu" href="javascript:void(0);" id="menu_search" onclick="showMenu(this.id)"><i class="fa fa-search" aria-hidden="true"></i> Search</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -38,18 +38,34 @@ $usertype = new Usertype();
 
 <div class="framepage" id="framepage"></div>
 <link rel="stylesheet" href="/musify/pages/CSS/webplayer.css">
-<hr>
-<h2>Custom player:</h2>
 <div class="webplayer-container" id="webplayer-container" style="color:black;">
     <audio preload="metadata">
         <source src="" preload="metadata" type="audio/mp3" id="sourceControl">
     </audio>
-    <button id="playpause" class="playerbutton"></button>
-    <span id="current-time" class="time">0:00</span>
-    <input type="range" id="music-slider" max="100" value="0">
-    <span id="duration" class="time">0:00</span>
-    <input type="range" id="volume-slider" max="100" value="70">
-    <button id="mute-icon" class="playerbutton"></button>
+    <table class="webplayer-table">
+        <tr>
+          <td>
+            <div style="margin-left: 23%; margin-top: 20px;">
+              <button id="playpause" class="playerbutton"></button>
+              <span id="current-time" class="time">0:00</span>
+              <input type="range" id="music-slider" max="100" value="0">
+              <span id="duration" class="time">0:00</span>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div style="margin-left: 34%;">
+              <button id="mute-icon" class="playerbutton"></button>
+              <input type="range" id="volume-slider" max="100" value="70">
+            </div>
+          </td>
+        </tr>
+        
+    </table>
+    
+    
+    
 </div>
 
 <script src="/musify/pages/JS/webplayer.js" type="module"></script>
